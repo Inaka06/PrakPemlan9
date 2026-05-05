537b5f6b01ad35636c36b5f9db1c23fb93d7cee4
Inaka06 <muhkaka06@gmail.com>
2026-05-05 12:00:26 +0700

    initial commit(soal awal)

---
+++ Code/src/mainFrame.java
@@ -0,0 +1,10 @@
+public class mainFrame {
+    public static void main(String[] args) {
+        TFrame window = new TFrame();
+
+        window.setTitle("Hitung Nilai Akhir dengan GUI");
+        window.setSize(350, 300);
+
+        window.setVisible(true);
+    }
+}
